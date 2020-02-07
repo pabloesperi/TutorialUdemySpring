1c49934cbc6aa65be60cd830b670c703a04e497e
Pablo Esperi <pabloesperi@hotmail.com>
2020-02-07 22:39:30 +0100

    tercero commit

--- src/main/java/com/tutorial/spring/controllers/IndexController.java
+++ src/main/java/com/tutorial/spring/controllers/IndexController.java
@@ -11,12 +11,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class IndexController {
 	
-	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	@RequestMapping(value = {"/index", "/", "/index2"}, method = RequestMethod.GET)
 	//se puede usar value o path. es igual.
 	//Si no se indica el método, por defecto es GET.
 	//Si es un GET se puede directamente anotar @GetMapping. También según el caso puede ser @PostMapping, etc.
 	public String index	(Model model) {
 		
+		model.addAttribute("Spring", "Prácticas Spring");
 		model.addAttribute("Nombre", "Mi nombre es Pablo");
 		
 		return "index";
