2bc00e07bfd1b4b387ee2658259afde44c41e535
Pablo Esperi <pabloesperi@hotmail.com>
2020-02-07 22:24:04 +0100

    segundo commit

--- src/main/java/com/tutorial/spring/controllers/IndexController.java
+++ src/main/java/com/tutorial/spring/controllers/IndexController.java
@@ -3,13 +3,18 @@ package com.tutorial.spring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+//IMPORTANTE: SI UNA VEZ QUE LEVANTAMOS EL SERVIDOR, LO STARTEAMOS DE VUELTA, VA A HABER CONFLICTO DE PUERTO. HAY QUE HACER 
+//RE START.
 
 @Controller
 public class IndexController {
 	
-	@RequestMapping(value = "/index")
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	//se puede usar value o path. es igual.
 	//Si no se indica el método, por defecto es GET.
+	//Si es un GET se puede directamente anotar @GetMapping. También según el caso puede ser @PostMapping, etc.
 	public String index	(Model model) {
 		
 		model.addAttribute("Nombre", "Mi nombre es Pablo");
